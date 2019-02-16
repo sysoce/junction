a39b997f0bc136346bc1b109e0045226a49c1e0e
Eco Sys <sysoce@gmail.com>
2019-02-17 05:04:18 +0900

    mock data from api (cause no CORS)

--- frontend/visualization/plot.js
+++ frontend/visualization/plot.js
@@ -1,18 +1,33 @@
-userId = '1915Hall';
+// userId = '1915Hall';
 
-// Make a request for a user with a given ID
-axios.get('http://ec2-3-18-9-170.us-east-2.compute.amazonaws.com/get_past_consumption/' + userId, { crossdomain: true })
-	.then(function (response) {
-	// handle success
+// // Make a request for a user with a given ID
+// axios.get('http://ec2-3-18-9-170.us-east-2.compute.amazonaws.com/get_past_consumption/' + userId, { crossdomain: true })
+// 	.then(function (response) {
+// 	// handle success
+// 	console.log(response);
+// 	})
+// 	.catch(function (error) {
+// 	// handle error
+// 	console.log(error);
+// 	})
+
+function httpGetAsync(theUrl, callback)
+{
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.onreadystatechange = function() {
+        if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
+            callback(xmlHttp.responseText);
+    }
+    xmlHttp.open("GET", theUrl, true); // true for asynchronous
+    xmlHttp.send(null);
+}
+
+httpGetAsync('./api-past-consumption-1915Hall', function(response) {
 	console.log(response);
-	})
-	.catch(function (error) {
-	// handle error
-	console.log(error);
-	})
+});
 
 
-Plotly.d3.csv("https://raw.githubusercontent.com/sysoce/junction/master/old_scripts/data/buildings-2018-3.csv", function(err, rows){
+Plotly.d3.csv("https://raw.githubusercontent.com/sysoce/junction/master/data/buildings-2018-3.csv", function(err, rows){
 	function unpack(rows, key) {
 		return rows.map(function(row) { return row[key]; });
 	}
