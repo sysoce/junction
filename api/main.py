3d2d60df324699f10d2de033de121e8940cff5b2
Caio <caio@ai2-jp.com>
2019-02-16 20:03:03 +0900

    Added a few more endpoints to the API, added scripts to add initial data

--- api/main.py
+++ api/main.py
@@ -35,11 +35,13 @@ class ElectricityManager(object):
 	
     @cherrypy.expose
     @cherrypy.tools.json_out()
-    def get_past_production(self, user_id):
-        # 2019-02-01T04:00:00Z
+    def get_past_production(self, user_id=None):
         session = Session()
         try:
-            past_production = session.query(Production).order_by(Production.id).filter_by(user_id=user_id).all()
+            if user_id:
+                past_production = session.query(Production).order_by(Production.id).filter_by(user_id=user_id).all()
+            else:
+                past_production = session.query(Production).order_by(Production.id).all()
             result = [element.to_dict() for element in past_production]
             return result
         finally:
@@ -47,11 +49,41 @@ class ElectricityManager(object):
 
     @cherrypy.expose
     @cherrypy.tools.json_out()
-    def get_past_consumption(self, user_id):
-        # 2019-02-01T04:00:00Z
+    def get_production_forecast(self, user_id=None):
         session = Session()
         try:
-            past_consumption = session.query(Consumption).order_by(Consumption.id).filter_by(user_id=user_id)
+            if user_id:
+                production_forecast = session.query(ProductionForecast).order_by(ProductionForecast.id).filter_by(user_id=user_id).all()
+            else:
+                production_forecast = session.query(ProductionForecast).order_by(ProductionForecast.id).all()
+            result = [element.to_dict() for element in production_forecast]
+            return result
+        finally:
+            session.close()
+
+    @cherrypy.expose
+    @cherrypy.tools.json_out()
+    def get_consumption_forecast(self, user_id=None):
+        session = Session()
+        try:
+            if user_id:
+                consumption_forecast = session.query(ConsumptionForecast).order_by(ConsumptionForecast.id).filter_by(user_id=user_id).all()
+            else:
+                consumption_forecast = session.query(ConsumptionForecast).order_by(ConsumptionForecast.id).all()
+            result = [element.to_dict() for element in consumption_forecast]
+            return result
+        finally:
+            session.close()
+
+    @cherrypy.expose
+    @cherrypy.tools.json_out()
+    def get_past_consumption(self, user_id=None):
+        session = Session()
+        try:
+            if user_id:
+                past_consumption = session.query(Consumption).order_by(Consumption.id).filter_by(user_id=user_id).all()
+            else:
+                past_consumption = session.query(Consumption).order_by(Consumption.id).all()
             result = [element.to_dict() for element in past_consumption]
             return result
         finally:
@@ -71,6 +103,27 @@ class ElectricityManager(object):
             production_db_entry = Production(user_id=input_json["user_id"], power=input_json["power"], timestamp=date_time_obj)
             session.add(production_db_entry)
             session.commit()
+            return {"message": "success"}
+        except:
+            session.rollback()
+            raise
+        finally:
+            session.close()
+
+    @cherrypy.expose
+    @cherrypy.tools.json_out()
+    @cherrypy.tools.json_in()
+    @cherrypy.tools.allow(methods=['POST'])
+    def insert_production_forecast(self):
+        input_json = cherrypy.request.json
+        date_time_str = input_json["timestamp"]
+        date_time_obj = datetime.datetime.strptime(date_time_str, TIMESTAMP_FORMAT)
+        session = Session()
+        try:
+            production_forecast_db_entry = ProductionForecast(user_id=input_json["user_id"], power=input_json["power"], timestamp=date_time_obj)
+            session.add(production_forecast_db_entry)
+            session.commit()
+            return {"message": "success"}
         except:
             session.rollback()
             raise
@@ -91,6 +144,28 @@ class ElectricityManager(object):
             consumption_db_entry = Consumption(user_id=input_json["user_id"], power=input_json["power"], timestamp=date_time_obj)
             session.add(consumption_db_entry)
             session.commit()
+            return {"message": "success"}
+        except:
+            session.rollback()
+            raise
+        finally:
+            session.close()
+
+    @cherrypy.expose
+    @cherrypy.tools.json_out()
+    @cherrypy.tools.json_in()
+    @cherrypy.tools.allow(methods=['POST'])
+    def insert_consumption_forecast(self):
+        input_json = cherrypy.request.json
+        # 2019-02-01T04:00:00Z
+        date_time_str = input_json["timestamp"]
+        date_time_obj = datetime.datetime.strptime(date_time_str, TIMESTAMP_FORMAT)
+        session = Session()
+        try:
+            consumption_forecast_db_entry = ConsumptionForecast(user_id=input_json["user_id"], power=input_json["power"], timestamp=date_time_obj)
+            session.add(consumption_forecast_db_entry)
+            session.commit()
+            return {"message": "success"}
         except:
             session.rollback()
             raise
